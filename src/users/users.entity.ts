0063be960688f44dadc1b5e758337e62740772e4
peanutDD <www.smoking.sexy@gmail.com>
2021-05-11 22:10:27 +0800

    add token

--- src/users/users.entity.ts
+++ src/users/users.entity.ts
@@ -2,6 +2,8 @@ import { Entity, Column, Index, BeforeInsert } from 'typeorm';
 import { Base } from '../shared/base.entity';
 import * as bcrypt from 'bcryptjs';
 import { Field, ObjectType } from '@nestjs/graphql';
+import * as jwt from 'jsonwebtoken';
+import config from '../config/configuration';
 
 @Entity('users')
 @ObjectType()
@@ -28,5 +30,11 @@ export class User extends Base {
   hashSyncPassword() {
     this.password = bcrypt.hashSync(this.password, 10);
     this.confirmedPassword = bcrypt.hashSync(this.confirmedPassword, 10);
+  }
+
+  @Field(() => String)
+  get token() {
+    const payload = { id: this.id, username: this.username };
+    return jwt.sign(payload, config.auth.secretKey, { expiresIn: '7D' });
   }
 }
